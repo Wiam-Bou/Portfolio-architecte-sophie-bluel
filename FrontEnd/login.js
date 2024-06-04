eb3565ef74ba0e9a7903e79d3dd0cc35332ac9e0
wiam <bouch.wiam@gmail.com>
2024-06-04 12:32:07 +0200

    modification login page

---
+++ FrontEnd/login.js
@@ -0,0 +1,50 @@
+const form = document.querySelector("form");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+// Récupération du span avec la classe error-msg
+const error = document.querySelector(".error-msg");
+
+// Initialisation du message d'erreur
+error.innerText = "";
+
+// Création de la fonction de redirection vers la page d'accueil
+function redirectToHome() {
+  window.location.href = "./index.html";
+}
+
+// Ajout d'un écouteur d'événement pour la soumission du formulaire
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); // Pour prévenir le comportement par défaut du navigateur (soumission auto)
+
+  // Récupération des valeurs des champs du formulaire
+  const users = {
+    email: emailInput.value,
+    password: passwordInput.value,
+  };
+
+  const requestData = JSON.stringify(users);  
+  console.log(requestData);
+
+  fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: requestData,
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("Erreur lors de la requête");
+    } else {
+      return response.json();  
+    }
+  })
+  .then((data) => {
+    sessionStorage.setItem("token", data.token); // Assume the token is in the `token` field of the response
+    redirectToHome();
+  })
+  .catch((error) => {
+    console.log(error);
+    error.innerText = "Échec de la connexion. Veuillez vérifier vos informations.";
+  });
+});
